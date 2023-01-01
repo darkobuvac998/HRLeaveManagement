0357751bf2f3e078a30175f18170c74ebe1e23b9
darko.buvac <buvac18@gmail.com>
2023-01-01 22:27:45 +0100

    added dbcontext factory class

---
+++ HRLeaveManagement.Persistance/LeaveManagementDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace HRLeaveManagement.Persistance
+{
+    public class LeaveManagementDbContextFactory : IDesignTimeDbContextFactory<LeaveManagementDbContext>
+    {
+        public LeaveManagementDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<LeaveManagementDbContext>();
+            var connectionString = configuration.GetConnectionString("Db");
+
+            builder.UseSqlServer(connectionString);
+
+            return new LeaveManagementDbContext(builder.Options);
+        }
+    }
+}
