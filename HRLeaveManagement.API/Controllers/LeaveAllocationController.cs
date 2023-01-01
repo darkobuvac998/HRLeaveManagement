c4766e6675b3e57a5aadf6b6d05f493682040116
darko.buvac <buvac18@gmail.com>
2023-01-02 00:34:15 +0100

    add leave allocation controller and delete command hanlder related to the leave allocation entity

---
+++ HRLeaveManagement.API/Controllers/LeaveAllocationController.cs
@@ -0,0 +1,63 @@
+﻿using HRLeaveManagement.Application.DTOs.LeaveAllocation;
+using HRLeaveManagement.Application.Features.LeaveAllocation.Requests.Commands;
+using HRLeaveManagement.Application.Features.LeaveAllocation.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveAllocationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveAllocationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<LeaveAllocationDto>>> GetLeaveAllocationsAsync()
+        {
+            var leaveAllocations = await _mediator.Send(new GetLeaveAllocationListRequest());
+            return Ok(leaveAllocations);
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult<LeaveAllocationDto>> GetLeaveAllocationAsync(int id)
+        {
+            var leaveAllocation = await _mediator.Send(new GetLeaveAllocationDetailsRequest { Id = id });
+            return Ok(leaveAllocation);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateLeaveAllocationAsync([FromBody] CreateLeaveAllocationDto createLeaveAllocationDto)
+        {
+            var command = new CreateLeaveAllocationCommand { CreateLeaveAllocationDto = createLeaveAllocationDto };
+            var response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateLeaveAllocationAsync([FromBody] UpdateLeaveAllocationDto updateLeaveAllocationDto)
+        {
+            var command = new UpdateLeaveAllocationCommand { UpdateLeaveAllocationDto = updateLeaveAllocationDto };
+            var response = await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("id")]
+        public async Task<ActionResult> DeleteLeaveAllocationAsync(int id)
+        {
+            var command = new DeleteLeaveAllocationCommand { Id = id };
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}
---
+++ HRLeaveManagement.Application/Features/LeaveAllocation/Handlers/Commands/DeleteLeaveAllocationCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Application.Exceptions;
+using HRLeaveManagement.Application.Features.LeaveAllocation.Requests.Commands;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Features.LeaveAllocation.Handlers.Commands
+{
+    public class DeleteLeaveAllocationCommandHandler : IRequestHandler<DeleteLeaveAllocationCommand, Unit>
+    {
+        private readonly IRepositoryManager _repository;
+
+        public DeleteLeaveAllocationCommandHandler(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+        public async Task<Unit> Handle(DeleteLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = await _repository.LeaveAllocation.GetAsync(request.Id);
+
+            if (leaveAllocation == null)
+                throw new NotFoundException(nameof(LeaveAllocation), request.Id);
+
+            await _repository.LeaveAllocation.DeleteAsync(leaveAllocation);
+
+            return Unit.Value;
+        }
+    }
+}
