c915579d3cab13a792f51a6b1284dde02fe8fd29
darko.buvac <buvac18@gmail.com>
2023-01-24 22:12:37 +0100

    identity

---
+++ HRLeaveManagement.Identity/IdentityServiceRegistration.cs
@@ -0,0 +1,77 @@
+﻿using HRLeaveManagement.Application.Contracts.Identity;
+using HRLeaveManagement.Application.Models.Identity;
+using HRLeaveManagement.Identity.Models;
+using HRLeaveManagement.Identity.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Identity
+{
+    public static class IdentityServiceRegistration
+    {
+        public static IServiceCollection ConfigureIdentityServices(
+            this IServiceCollection services,
+            IConfiguration configuration
+        )
+        {
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+
+            services.AddDbContext<LeaveManagementIdentityDbContext>(
+                options =>
+                    options.UseNpgsql(
+                        configuration.GetConnectionString("Db_Auth"),
+                        b =>
+                            b.MigrationsAssembly(
+                                typeof(LeaveManagementIdentityDbContext).Assembly.FullName
+                            )
+                    )
+            );
+
+            services
+                .AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<LeaveManagementIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddTransient<IAuthService, AuthService>();
+
+            services
+                .AddAuthentication(
+                    options =>
+                    {
+                        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    }
+                )
+                .AddJwtBearer(
+                    options =>
+                    {
+                        options.TokenValidationParameters =
+                            new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                            {
+                                ValidateIssuerSigningKey = true,
+                                ValidateIssuer = true,
+                                ValidateAudience = true,
+                                ValidateLifetime = true,
+                                ClockSkew = TimeSpan.Zero,
+                                ValidIssuer = configuration["JwtSettings:Issuer"],
+                                ValidAudience = configuration["JwtSettings:Audience"],
+                                IssuerSigningKey = new SymmetricSecurityKey(
+                                    Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"])
+                                )
+                            };
+                    }
+                );
+
+            return services;
+        }
+    }
+}
